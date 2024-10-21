94f3dc675c582333fcc5d7d88418095bc93effaf
Amal Jinan <107645053+AmalJinan@users.noreply.github.com>
2024-10-21 07:38:07 -0700

    Update GameLevelWater.js

--- assets/js/rpg2x/GameLevelWater.js
+++ assets/js/rpg2x/GameLevelWater.js
@@ -35,12 +35,12 @@ class GameLevelWater {
         STEP_FACTOR: 1000,
         ANIMATION_RATE: 50,
         INIT_POSITION: { x: 0, y: height - (height/TURTLE_SCALE_FACTOR) }, 
-        pixels: {height: 280, width: 256},
-        orientation: {rows: 4, columns: 3 },
-        down: {row: 0, start: 0, columns: 3 },
-        left: {row: 1, start: 0, columns: 3 },
-        right: {row: 2, start: 0, columns: 3 },
-        up: {row: 3, start: 0, columns: 3 },
+        pixels: {height: 183, width: 275},
+        orientation: {rows: 4, columns: 6 },
+        down: {row: 0, start: 0, columns: 6 },
+        left: {row: 1, start: 0, columns: 6 },
+        right: {row: 2, start: 0, columns: 6 },
+        up: {row: 3, start: 0, columns: 6 },
     };
 
     // Player 2 sprite data (fish)
